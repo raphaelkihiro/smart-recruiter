ef23710156a6a8c17af73b102deb61820e5f641a
raphael kihiro <kihirongigi@gmail.com>
2025-07-17 11:55:17 +0300

    created the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* <Route path="/login" element={<Login/>} /> */}
+          <Route path="/signup" element={<Signup/>} />
         </Routes>
       </Router>
     </>
